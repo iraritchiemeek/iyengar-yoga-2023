b07c5a28a13a11b376ed909718e891f3d55077b6
iraritchiemeek <iraritchiemeek@gmail.com>
2023-04-19 21:47:51 +0900

    add grid

---
+++ components/bgGrid.js
@@ -0,0 +1,19 @@
+import {Container, Row, Col} from 'react-bootstrap';
+
+function BgGrid() {
+
+  return (
+  	<Container id="bg-grid">
+  		<Row id="grid">
+  		  <Col></Col>
+  		  <Col></Col>
+  		  <Col></Col>
+  		  <Col></Col>
+  		  <Col></Col>
+  		  <Col></Col>
+  		</Row>
+  	</Container>
+  );
+}
+
+export default BgGrid;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from 'components/header'
+import BgGrid from 'components/bgGrid'
 import qs from 'qs';
 import ReactMarkdown from 'react-markdown';
 
@@ -34,17 +35,15 @@ export const getStaticProps = async () => {
 };
 
 function renderImageSection(images) {
-  console.log(images)
   switch(images.length){
     case 1:
       break
     case 2:
       return(
-        <Row className="pt-10">
+        <Row className="pt-10 image-section">
           <Col xs={12} md={6}>
             <Image
               src={images[0].attributes.url}
-              style={{objectFit: 'contain', maxWidth: '100%', maxHeight: '420px'}}
               width={images[0].attributes.width}
               height={images[0].attributes.height}
             />
@@ -53,7 +52,6 @@ function renderImageSection(images) {
           <Col xs={12} md={4}>
             <Image
               src={images[1].attributes.url}
-              style={{objectFit: 'contain', maxWidth: '100%',  maxHeight: '420px'}}
               width={images[1].attributes.width}
               height={images[1].attributes.height}
             />
@@ -67,19 +65,23 @@ function renderImageSection(images) {
 }
 
 function renderSection(section) {
-  console.log(section)
   switch (section.__component) {
     case 'images.images':
       return renderImageSection(section.images.data)
       break;
 
     case 'text.title-content-content':
-      console.log(section)
       return (
         <Row className="pt-10">
-          <Col xs={12} md={3} ><h2 className="sticky-top">{section.Title}</h2></Col>
-          <Col xs={12} md={3} ><ReactMarkdown className="sticky-top">{section.center_content}</ReactMarkdown></Col>
-          <Col xs={12} md={6}><ReactMarkdown>{section.main_content}</ReactMarkdown></Col>
+          <Col></Col>
+          <Col xs={12} md={8}>
+            <Row>
+              <Col xs={12} md={3} ><h2 className="sticky-top">{section.Title}</h2></Col>
+              <Col xs={12} md={3} ><ReactMarkdown className="sticky-top">{section.center_content}</ReactMarkdown></Col>
+              <Col xs={12} md={6}><ReactMarkdown>{section.main_content}</ReactMarkdown></Col>
+            </Row>
+          </Col>
+          <Col></Col>
         </Row>
       )
       break;
@@ -90,25 +92,27 @@ function renderSection(section) {
 }
 
 export default function HomePage({ page }) {
-  console.log(page)
   const banner_image = page.banner_image.data.attributes
   return (
-    <main>
-      <Container>
-        <Header />
-      </Container>
-      <div className="w-100">
-        <Image
-          src={banner_image.url}
-          style={{objectFit: 'cover', height: '750px', maxWidth: '100%'}}
-          width={banner_image.width}
-          height={banner_image.height}
-          priority
-        />
-      </div>
-      <Container>
-        {page.content.map(section => renderSection(section))}
-      </Container>
-    </main>
+    <>
+      <main>
+        <BgGrid />
+        <Container>
+          <Header />
+        </Container>
+        <div className="w-100">
+          <Image
+            src={banner_image.url}
+            style={{objectFit: 'cover', height: '750px', maxWidth: '100%'}}
+            width={banner_image.width}
+            height={banner_image.height}
+            priority
+          />
+        </div>
+        <Container>
+          {page.content.map(section => renderSection(section))}
+        </Container>
+      </main>
+    </>
   );
 }

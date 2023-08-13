bf7372d2c910e133c85b162de8fc62726fe6d8eb
iraritchiemeek <iraritchiemeek@gmail.com>
2023-08-13 16:31:34 +0900

    add flyers

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,4 @@
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}) {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body>{children}</body>

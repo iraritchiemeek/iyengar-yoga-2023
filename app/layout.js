c4f301eadff53241491a98249e9cafa485109254
iraritchiemeek <iraritchiemeek@gmail.com>
2023-09-04 20:21:52 +0900

    import font

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,21 @@ import Header from 'components/header'
 import Footer from 'components/footer'
 import ScrollProgressBar from 'components/scrollProgressBar'
 import { Analytics } from '@vercel/analytics/react';
+import localFont from 'next/font/local'
 
+const moderat = localFont({
+  src: [
+    {
+      path: '../fonts/Moderat/Moderat-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Moderat/Moderat-Bold.woff2',
+      weight: '800',
+    },
+  ],
+})
 
 export const metadata = {
   title: 'Home',
@@ -14,7 +28,7 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <body>
+      <body className={moderat.className}>
         <BgGrid />
         <ScrollProgressBar />
         {/*<Header headerHeight={headerHeight} setHeaderHeight={setHeaderHeight} headerMinified={headerMinified} setHeaderMinified={setHeaderMinified} />*/}

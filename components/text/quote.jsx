96bb3331ec1e6093d75e9e795dfe65df37f5de9b
iraritchiemeek <iraritchiemeek@gmail.com>
2024-01-15 09:20:25 +1300

    hide retreats. update quote padding

--- components/text/quote.jsx
+++ components/text/quote.jsx
@@ -1,7 +1,7 @@
 export const Quote = ({section}) => {
 	return (
-		<div className='md:col-span-4 md:col-start-2'>
-		  <p className="text-4xl font-bold leading-10">“{section.Quote}”</p>
+		<div className='md:col-span-4 md:col-start-2 md:py-40'>
+		  <p className="text-xl md:text-4xl font-bold md:leading-10">“{section.Quote}”</p>
 		  <p className="font-bold text-xl pt-4">- {section.Author}</p>
 		</div>
 	)
